6abd0b74a90158951e068fbeaaa5502f9a431be0
littlesea96 <soraya.sakonwittayanon@gmail.com>
2016-05-27 22:26:24 +0700

    fixed point

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -94,6 +94,10 @@ struct thread
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
 
+    // add
+    int nice_value;
+    int recent_cpu;
+
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
